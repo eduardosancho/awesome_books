b2f9bae3a993c75d5d0c13b769f75eef716ebadb
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-01-03 16:31:02 -0600

    Display new books

--- scripts/booklist.js
+++ scripts/booklist.js
@@ -26,34 +26,47 @@ addButton.addEventListener('click', addBook);
 
 function getBook() {
     const bookCard = {
-        
+
         title: document.getElementById('book-title-input').value,
-        author:  document.getElementById('book-author-input').value
+        author: document.getElementById('book-author-input').value
     }
     return bookCard
 }
 
 
 function addBook() {
-    const book=getBook()
+    const book = getBook();
+    if ()
 
     books.push(book);
-    storeData()
-    displayBook()
+    storeData();
+    displayBooks(books);
 }
 
 
-function displayBook()  {
-    for (bookCard in books) {    
-        const bookCard = document.createElement('div');
-        bookCard.innerHTML = `
-        <h2 class="book-title">${bookCard.title}</h2>
-        <h3 class="book-author">${bookCard.author}</h3>
-        <button class="remove-button" type="button">Remove</button>
-        `;    
-        document.getElementById('book-list').appendChild(bookCard);
+function displayBook() {
+
+    console.log(books.length);
+    if (books.length > 0) {
+        for (let i = 0; i < books.length; i++) {
+            document.getElementById('book-list').removeChild(document.querySelector('tr'));
+        }
     }
-    
-} 
+}
+
+function displayBooks(arr) {
+    const bookList = document.getElementById('book-list');
+    arr.forEach((b) => {
+        const rowHTML = (b) => `
+        <h2 class="book-title">${b.title}</h2>
+        <h3 class="book-author">${b.author}</h3>
+        <button class="remove-button" type="button">Remove</button>
+        `
+        const rowTr = document.createElement('tr');
+        rowTr.innerHTML = rowHTML(b);
+        document.getElementById('book-list').appendChild(rowTr);
+    })
+}
+
 
 
