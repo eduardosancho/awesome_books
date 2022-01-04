ecd7cfbe84cb11983575749c156fb00e93cabbbb
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-01-04 16:32:11 -0600

    Fix Linters and bookcard display

--- scripts/booklist-classes.js
+++ scripts/booklist-classes.js
@@ -1,30 +1,30 @@
+/* eslint max-classes-per-file: ["error", 2] */
 class Book {
   constructor() {
     this.title = document.getElementById('book-title-input').value;
-    this.author = document.getElementById('book-author-input').value;  
+    this.author = document.getElementById('book-author-input').value;
   }
 }
 
 class Bookshelf {
-  constructor(){
-      this.books = localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
-      this.bookList = document.getElementById('book-list');
+  constructor() {
+    this.books = localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
+    this.bookList = document.getElementById('book-list');
   }
 
   removeBook(index) {
     this.books = this.books.filter((book) => this.books.indexOf(book) !== index);
-    localStorage.books = JSON.stringify(this.books);
+    this.storeData();
     // eslint-disable-next-line no-use-before-define
     this.displayBooks(this.books);
   }
-  
+
   displayBooks() {
     this.bookList.innerHTML = '';
-  
+
     this.books.forEach((book) => {
       const rowHTML = (book) => `
-            <h2 class="book-title">${book.title}</h2>
-            <h3 class="book-author">${book.author}</h3>
+            <span class="book-title">"${book.title}" by ${book.author}</span>
             <button class="remove-button" type="button">Remove</button>
             `;
       const rowTr = document.createElement('tr');
@@ -35,9 +35,9 @@ class Bookshelf {
       document.getElementById('book-list').appendChild(rowTr);
     });
   }
-  
-  storeData(books) {
-    localStorage.setItem('books', JSON.stringify(books));
+
+  storeData() {
+    localStorage.setItem('books', JSON.stringify(this.books));
   }
 
   addBook() {
@@ -45,7 +45,7 @@ class Bookshelf {
     if (JSON.stringify(this.books[this.books.length - 1]) !== JSON.stringify(book)
         && book.title !== '' && book.author !== '') {
       this.books.push(book);
-      this.storeData(this.books);
+      this.storeData();
       this.displayBooks(this.books);
     }
   }
@@ -53,5 +53,5 @@ class Bookshelf {
 
 const bookShelf = new Bookshelf();
 const addButton = document.getElementById('add-book-button');
-addButton.addEventListener('click', () => {bookShelf.addBook()});
+addButton.addEventListener('click', () => { bookShelf.addBook(); });
 bookShelf.displayBooks();
