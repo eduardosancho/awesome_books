2a9bdcacf026da36494796e58f194a2976f27a75
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-01-05 11:03:41 -0600

    Add active menu-link color

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -4,17 +4,23 @@ function startTime() {
 }
 
 const selectList = document.getElementById('list');
-selectList.addEventListener('click', () => { toggleDisplay("awesome-list") });
+selectList.addEventListener('click', () => { toggleDisplay("awesome-list", selectList) });
 
 const selectNew = document.getElementById('new');
-selectNew.addEventListener('click', () => { toggleDisplay("add-book") });
+selectNew.addEventListener('click', () => { toggleDisplay("add-book", selectNew) });
 
 const selectContact = document.getElementById('contact');
-selectContact.addEventListener('click', () => { toggleDisplay("contact-info") });
+selectContact.addEventListener('click', () => { toggleDisplay("contact-info", selectContact) });
 
-function toggleDisplay(id) {
+function toggleDisplay(id, link) {
     var element = document.querySelectorAll('.content');
     element.forEach(section => section.classList.add("hide"));
     var current = document.getElementById(id);
     current.classList.remove("hide");
-  }
+
+    var menuLinks = document.querySelectorAll('.menu-links');
+    menuLinks.forEach(selector => selector.classList.remove('active'))
+    link.classList.add('active');
+}
+
+toggleDisplay("awesome-list", selectList)
