a3c36d2f1d4174f02c98d84a1294c9683aec0f6f
gdumani <gdumani@gmail.com>
2022-01-05 10:24:14 -0600

    add navbar selection variables and toggle function

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -2,5 +2,14 @@ function startTime() {
     document.getElementById('date').innerHTML = new Date();
     setTimeout(startTime, 1000);
 }
+const selectList = document.getElementById('list');
+const selectNew = document.getElementById('new');
+const selectContact = document.getElementById('contact');
 
-console.log("hello");
+
+
+
+function toggleDisplay() {
+    var element = document.getElementById("myDIV");
+    element.classList.toggle("mystyle");
+  }
