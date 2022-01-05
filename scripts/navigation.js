4ec059ad5da20c3d29754d0da3ec9204bae133be
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-01-05 11:15:56 -0600

    Check Linters

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,26 +1,27 @@
 function startTime() {
-    document.getElementById('date').innerHTML = new Date();
-    setTimeout(startTime, 1000);
+  document.getElementById('date').innerHTML = new Date();
+  setTimeout(startTime, 1000);
+}
+
+function toggleDisplay(id, link) {
+  const element = document.querySelectorAll('.content');
+  element.forEach((section) => section.classList.add('hide'));
+  const current = document.getElementById(id);
+  current.classList.remove('hide');
+
+  const menuLinks = document.querySelectorAll('.menu-links');
+  menuLinks.forEach((selector) => selector.classList.remove('active'));
+  link.classList.add('active');
 }
 
 const selectList = document.getElementById('list');
-selectList.addEventListener('click', () => { toggleDisplay("awesome-list", selectList) });
+selectList.addEventListener('click', () => { toggleDisplay('awesome-list', selectList); });
 
 const selectNew = document.getElementById('new');
-selectNew.addEventListener('click', () => { toggleDisplay("add-book", selectNew) });
+selectNew.addEventListener('click', () => { toggleDisplay('add-book', selectNew); });
 
 const selectContact = document.getElementById('contact');
-selectContact.addEventListener('click', () => { toggleDisplay("contact-info", selectContact) });
+selectContact.addEventListener('click', () => { toggleDisplay('contact-info', selectContact); });
 
-function toggleDisplay(id, link) {
-    var element = document.querySelectorAll('.content');
-    element.forEach(section => section.classList.add("hide"));
-    var current = document.getElementById(id);
-    current.classList.remove("hide");
-
-    var menuLinks = document.querySelectorAll('.menu-links');
-    menuLinks.forEach(selector => selector.classList.remove('active'))
-    link.classList.add('active');
-}
-
-toggleDisplay("awesome-list", selectList)
+startTime();
+toggleDisplay('awesome-list', selectList);
