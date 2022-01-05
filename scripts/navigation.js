e0fe0a335efd158721c1498e949d451d94d29d36
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-01-05 10:44:42 -0600

    Show selected section only

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -2,14 +2,19 @@ function startTime() {
     document.getElementById('date').innerHTML = new Date();
     setTimeout(startTime, 1000);
 }
+
 const selectList = document.getElementById('list');
+selectList.addEventListener('click', () => { toggleDisplay("awesome-list") });
+
 const selectNew = document.getElementById('new');
+selectNew.addEventListener('click', () => { toggleDisplay("add-book") });
+
 const selectContact = document.getElementById('contact');
+selectContact.addEventListener('click', () => { toggleDisplay("contact-info") });
 
-
-
-
-function toggleDisplay() {
-    var element = document.getElementById("myDIV");
-    element.classList.toggle("mystyle");
+function toggleDisplay(id) {
+    var element = document.querySelectorAll('.content');
+    element.forEach(section => section.classList.add("hide"));
+    var current = document.getElementById(id);
+    current.classList.remove("hide");
   }
